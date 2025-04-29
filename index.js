343e4e6daf15be8c1af4625419a43a0f5eb44610
leonardoalta <leonardoalta7688@gmail.com>
2025-04-28 18:52:47 -0600

    10

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ require('dotenv').config();
 const server = require('./server');
 const PORT = process.env.PORT || 3300;
 
+console.log(`Starting server on port ${PORT}…`);
+app.listen(PORT, () => {
+  console.log(`✅ Server is live at http://0.0.0.0:${PORT}`);
+});
